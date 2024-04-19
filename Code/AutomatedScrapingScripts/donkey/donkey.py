396d60767ef0570b4607438cdaf2fed9b11dc5fc
Sameer Patel <sameer.patel201999@gmail.com>
2024-04-19 10:54:08 -0300

    Update donkey.py

--- Code/AutomatedScrapingScripts/donkey/donkey.py
+++ Code/AutomatedScrapingScripts/donkey/donkey.py
@@ -70,8 +70,8 @@ df = pd.read_csv("rentdonkey.csv")
 import boto3
 from io import StringIO
 session = boto3.Session(
-    aws_access_key_id='AKIAU6GD24KAGUDG56MR',
-    aws_secret_access_key='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'
+    aws_access_key_id='',
+    aws_secret_access_key=''
 )
 
 s3 = session.client('s3')
@@ -80,17 +80,17 @@ def upload_file_to_s3(bucket_name, file_name, data_frame):
     data_frame.to_csv(csv_buffer, index=False)
     s3.put_object(Bucket=bucket_name, Key=file_name, Body=csv_buffer.getvalue())
 
-bucket_name = 'mcda-hackathon-s3-bucket'
+bucket_name = ''
 file_name_in_s3 = 'ScrapingInput/Rentdonkey/rentdonkey.csv'
 upload_file_to_s3(bucket_name, file_name_in_s3, df)
 print(f'File {file_name_in_s3} uploaded to {bucket_name}')
 
 if True:
     config = {
-        'user': 'admin',
-        'password': 'mcdaPassword',
-        'host': 'mcdahackathondb.cp0g6cm4mpmv.ca-central-1.rds.amazonaws.com',
-        'database': 'MCDAHackathon'
+        'user': '',
+        'password': '',
+        'host': '',
+        'database': ''
     }
 
 
