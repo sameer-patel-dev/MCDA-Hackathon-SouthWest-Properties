c3048cfe4f332cbb758f97c8fefc90c4798b2b69
Sameer Patel <sameer.patel201999@gmail.com>
2024-04-19 10:57:16 -0300

    Update rent_fast.py

--- Code/AutomatedScrapingScripts/rent_fast/rent_fast.py
+++ Code/AutomatedScrapingScripts/rent_fast/rent_fast.py
@@ -60,8 +60,8 @@ if response.status_code == 200:
     import boto3
     from io import StringIO
     session = boto3.Session(
-        aws_access_key_id='AKIAU6GD24KAGUDG56MR',
-        aws_secret_access_key='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'
+        aws_access_key_id='',
+        aws_secret_access_key=''
     )
 
     s3 = session.client('s3')
@@ -70,7 +70,7 @@ if response.status_code == 200:
         data_frame.to_csv(csv_buffer, index=False)
         s3.put_object(Bucket=bucket_name, Key=file_name, Body=csv_buffer.getvalue())
 
-    bucket_name = 'mcda-hackathon-s3-bucket'
+    bucket_name = ''
     file_name_in_s3 = 'ScrapingInput/Rent_Fast/rent_fast.csv'
     upload_file_to_s3(bucket_name, file_name_in_s3, df)
     print(f'File {file_name_in_s3} uploaded to {bucket_name}')
@@ -83,10 +83,10 @@ else:
 
 if True:
     config = {
-        'user': 'admin',
-        'password': 'mcdaPassword',
-        'host': 'mcdahackathondb.cp0g6cm4mpmv.ca-central-1.rds.amazonaws.com',
-        'database': 'MCDAHackathon'
+        'user': '',
+        'password': '',
+        'host': '',
+        'database': ''
     }
 
 
